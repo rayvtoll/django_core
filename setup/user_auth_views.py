8975e01b6ddeebf40272e0074962f2a750ffd361
rayvtoll <r.v.toll@jio.nl>
2024-01-09 15:03:17 +0100

    added serializer + MFA

--- setup/user_auth_views.py
+++ setup/user_auth_views.py
@@ -4,17 +4,18 @@ from keycloak import KeycloakOpenID, exceptions, KeycloakPostError
 from typing import Any
 
 from django.conf import settings
-from django.core.cache import cache
 from django.http import HttpResponse, JsonResponse
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .helpers import get_token_info
-
+from .serializers import LoginSerializer
 
 class Login(APIView):
     """Returns keycloak token at successful login"""
+
+    serializer_class = LoginSerializer
 
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
@@ -26,10 +27,14 @@ class Login(APIView):
         )
 
     def post(self, request, *args, **kwargs) -> HttpResponse:
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         try:
             new_token = self.keycloak_client.token(
-                username=request.data.get("username"),
-                password=request.data.get("password"),
+                username=serializer.data["username"],
+                password=serializer.data["password"],
+                totp=serializer.data.get("otp"),
             )
             access_token_info = get_token_info(new_token.get("access_token", ""))
 
@@ -44,6 +49,10 @@ class Login(APIView):
 
             return Response(data=new_token | access_token_info)
         except exceptions.KeycloakAuthenticationError as e:
+            return Response(
+                data=json.loads(e.error_message.decode()), status=e.response_code
+            )
+        except exceptions.KeycloakPostError as e:
             return Response(
                 data=json.loads(e.error_message.decode()), status=e.response_code
             )

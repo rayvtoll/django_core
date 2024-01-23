f6fb19a55866d790dc7838825ad8917e6798d485
rayvtoll <r.v.toll@jio.nl>
2024-01-23 11:26:15 +0100

    added cors headers + more .env in settings

--- run1_automatic.sh
+++ run1_automatic.sh
@@ -27,3 +27,15 @@ cp setup/user_auth_serializers.py project/apps/user_auth/serializers.py
 
 # remove database lines from settings
 sed -i "73d;74d;75d;76d;77d;78d;79d;80d;81d;82d;83d" project/settings/settings.py
+
+# replace lines in settings
+sed -i '1s/^/from decouple import config, Csv\n/' project/settings/settings.py
+sed -i '/^SECRET_KEY = /c\SECRET_KEY = config("SECRET_KEY")' project/settings/settings.py
+sed -i '/^BASE_DIR = /c\BASE_DIR = Path(__file__).resolve().parent.parent.parent' project/settings/settings.py
+sed -i '/^DEBUG = /c\DEBUG = config("DEBUG", default=False, cast=bool)' project/settings/settings.py
+sed -i '/^ALLOWED_HOSTS = /c\ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())' project/settings/settings.py
+
+# cors in settings
+echo 'CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())' >> project/settings/settings.py
+echo 'INSTALLED_APPS += ["corsheaders"]' >> project/settings/settings.py
+sed -i "/^    'django.middleware.common.CommonMiddleware',/c\    'corsheaders.middleware.CorsMiddleware', 'django.middleware.common.CommonMiddleware'," project/settings/settings.py
